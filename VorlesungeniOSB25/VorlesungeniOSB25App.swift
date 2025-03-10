760b238b24812cfa5a9901ed910050f318cc54d1
Felix Bücher <felix.buecher@syntax-institut.de>
2025-03-10 08:55:51 +0100

    Initial Commit

---
+++ VorlesungeniOSB25/VorlesungeniOSB25App.swift
@@ -0,0 +1,17 @@
+//
+//  VorlesungeniOSB25App.swift
+//  VorlesungeniOSB25
+//
+//  Created by Felix B on 10.03.25.
+//
+
+import SwiftUI
+
+@main
+struct VorlesungeniOSB25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
