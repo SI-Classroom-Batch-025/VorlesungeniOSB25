e5e73ee67b8431293648bfd477aada2eae41fc27
Felix Bücher <felix.buecher@syntax-institut.de>
2025-03-31 01:40:56 +0200

    Aufbereitung VL zur Woche 3

---
+++ 03. Navigation WH/NavigationWHApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationWHApp.swift
+//  03. Navigation WH
+//
+//  Created by Felix B on 31.03.25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationWHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
