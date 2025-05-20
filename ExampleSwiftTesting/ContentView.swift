8798f60cc2fb39d1a980e62a1a4753cefffa84d7
Felix Bücher <felix.buecher@syntax-institut.de>
2025-05-20 10:23:40 +0200

    VL 20.05

--- ExampleSwiftTesting/ContentView.swift
+++ ExampleSwiftTesting/ContentView.swift
@@ -10,16 +10,34 @@ import SwiftUI
 struct ContentView: View {
     
     private let notifications = NotificationService()
+    @State private var date = Date()
+    
+    var getTimeFromDate: [String] {
+        date.formatted(date: .omitted, time: .shortened).components(separatedBy: ":")
+    }
     
     var body: some View {
         VStack {
+            
+            Text(getTimeFromDate.description)
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             
+            DatePicker("Select Notification Time", selection: $date, displayedComponents: .hourAndMinute)
+            
             Button("Notifications!") {
                 notifications.requestPermission()
+            }
+            
+            Button("Send Timed Notification!") {
+                notifications.timedNotification()
+            }
+            
+            Button("Send Calendar Notification!") {
+                notifications.calendarNotification(hour: Int(getTimeFromDate[0]) ?? 0, minute: Int(getTimeFromDate[1]) ?? 0)
             }
         }
         .padding()
--- ExampleSwiftTesting/Service/NotificationService.swift
+++ ExampleSwiftTesting/Service/NotificationService.swift
@@ -26,4 +26,45 @@ class NotificationService {
             print("Thank you for giving permission.")
         }
     }
+    
+    func timedNotification() {
+        // 1. Content (Was?) wird erstellt
+        let content = UNMutableNotificationContent()
+        content.title = "Hello World!"
+        content.body = "Das ist eine Testnotification!"
+        content.sound = UNNotificationSound.default
+        
+        // 2. Trigger (Wann?) wird erstellt
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        // 3. Schicken
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { error in
+            if let error {
+                print("Fehler beim schicken der Notification! \(error)")
+            }
+        }
+    }
+    
+    func calendarNotification(hour: Int, minute: Int) {
+        // 1. Content (Was?) wird erstellt
+        let content = UNMutableNotificationContent()
+        content.title = "Hello World!"
+        content.body = "Das ist eine Testnotification!"
+        content.sound = UNNotificationSound.default
+        
+        // 2. Trigger (Wann?) wird erstellt
+        var date = DateComponents()
+        date.hour = hour
+        date.minute = minute
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+        // 3. Schicken
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request) { error in
+            if let error {
+                print("Fehler beim schicken der Notification! \(error)")
+            }
+        }
+    }
 }
