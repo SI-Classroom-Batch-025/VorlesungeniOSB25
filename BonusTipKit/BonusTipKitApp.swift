62f28de65faab9371ae4db5ac891e43968f63eb3
Felix Bücher <felix.buecher@syntax-institut.de>
2025-06-17 22:31:34 +0200

    Bonus TipKit Vorbereitung

---
+++ BonusTipKit/BonusTipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonusTipKitApp.swift
+//  BonusTipKit
+//
+//  Created by Felix B on 17.06.25.
+//
+
+import SwiftUI
+
+@main
+struct BonusTipKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
