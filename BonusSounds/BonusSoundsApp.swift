281904ea73a02f1788009cdbd841794919d50787
Felix Bücher <felix.buecher@syntax-institut.de>
2025-06-25 23:05:34 +0200

    Vorbereitung Bonus Sounds

---
+++ BonusSounds/BonusSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonusSoundsApp.swift
+//  BonusSounds
+//
+//  Created by Felix B on 25.06.25.
+//
+
+import SwiftUI
+
+@main
+struct BonusSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
